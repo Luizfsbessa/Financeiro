a42932480b785d13cb4a6a20ad006ff3c1743814
Luizfsbessa <luizfsbessa@gmail.com>
2026-08-27 16:32:24 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // persistência offline própria, cuidada pelo próprio SDK do Firebase.
 // ============================================================
 
-const CACHE_NOME = "bills-conciliacao-shell-v26";
+const CACHE_NOME = "bills-conciliacao-shell-v27";
 
 const ARQUIVOS_DO_SHELL = [
   "./",
